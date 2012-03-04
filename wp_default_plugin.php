ec4580f1242be6e7c132f8a99445be2ba4634a20
LoïcG <gerbaudloic@gmail.com>
2012-03-04 08:49:23 +0100

    Remove rights test on admin_settings (alreay done with add_options_page)

--- wp_default_plugin.php
+++ wp_default_plugin.php
@@ -91,9 +91,6 @@ class wp_default_plugin{
      * The admin settings page
      */
     public static function admin_settings(){
-   		if (!current_user_can('manage_options'))  {
-			wp_die( __('You do not have sufficient permissions to access this page.','wp_default_plugin') );
-		}
 		?>
 
 <div class="wrap">
